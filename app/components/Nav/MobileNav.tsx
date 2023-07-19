4ae3f3baf5769e53335b5043442e1d0e028c19c1
Thiago Temple <thitemple@MegaGigaMBP.local>
2023-07-19 09:13:04 -0400

    Updates RDT

--- app/components/Nav/MobileNav.tsx
+++ app/components/Nav/MobileNav.tsx
@@ -11,7 +11,7 @@ function MobileNavItem({
 	children: ReactNode;
 }) {
 	return (
-		<li className="border-b border-slate-200 py-4 last:border-0">
+		<li className="border-b border-slate-200 py-4 last:border-0 focus:bg-slate-300 dark:focus:bg-violet-500">
 			<Link to={to} autoFocus={autoFocus}>
 				{children}
 			</Link>
@@ -48,46 +48,48 @@ export function MobileNav({
 	}, []);
 
 	function handleClick(e: React.MouseEvent<HTMLDialogElement, MouseEvent>) {
-		if (e.target === e.currentTarget) {
+		if (e.target.id !== "mobile-nav") {
 			onClose();
 		}
 	}
 
 	return (
 		<dialog
-			className="backdrop:bg-gray-900/50 z-50 w-5/6 rounded-2xl bg-white px-4 py-6 backdrop:backdrop-blur-sm dark:bg-violet-700"
+			className="backdrop:bg-gray-900/50 z-50 w-5/6 rounded-2xl bg-slate-100 px-4 py-6 backdrop:backdrop-blur-sm dark:bg-violet-700"
 			onClick={handleClick}
 			ref={ref}
 		>
-			<header className="flex justify-between text-slate-500 dark:text-slate-200">
-				<h2>Navigation</h2>
-				<button onClick={onClose}>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth="1.5"
-						stroke="currentColor"
-						className="h-6 w-6"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M6 18L18 6M6 6l12 12"
-						/>
-					</svg>
-				</button>
-			</header>
-			<ul className="mt-6 flex flex-col text-lg text-slate-600 dark:text-slate-200">
-				<MobileNavItem to="/">Home</MobileNavItem>
-				<MobileNavItem to="/blog" autoFocus>
-					Blog
-				</MobileNavItem>
-				<MobileNavItem to="/courses">Courses</MobileNavItem>
-				{/* <MobileNavItem to="/speaking">Speaking</MobileNavItem>
+			<div id="mobile-nav">
+				<header className="flex justify-between text-slate-500 dark:text-slate-200">
+					<h2>Navigation</h2>
+					<button onClick={onClose}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="h-6 w-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M6 18L18 6M6 6l12 12"
+							/>
+						</svg>
+					</button>
+				</header>
+				<ul className="mt-6 flex flex-col text-lg text-slate-600 dark:text-slate-200">
+					<MobileNavItem to="/">Home</MobileNavItem>
+					<MobileNavItem to="/blog" autoFocus>
+						Blog
+					</MobileNavItem>
+					<MobileNavItem to="/courses">Courses</MobileNavItem>
+					{/* <MobileNavItem to="/speaking">Speaking</MobileNavItem>
 				<MobileNavItem to="/uses">Uses</MobileNavItem> */}
-				<MobileNavItem to="/about">About</MobileNavItem>
-			</ul>
+					<MobileNavItem to="/about">About</MobileNavItem>
+				</ul>
+			</div>
 		</dialog>
 	);
 }
